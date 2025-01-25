6b7688066eac1c6071fbfabf51b13df140b41b71
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2025-01-25 10:13:05 +0530

    Time: 1 ms (16.64%), Space: 40.5 MB (92.64%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.hammingWeight(11));  // Output: 3
+        System.out.println(solution.hammingWeight(128)); // Output: 1
+        System.out.println(solution.hammingWeight(2147483645)); // Output: 31
+    }
+}
