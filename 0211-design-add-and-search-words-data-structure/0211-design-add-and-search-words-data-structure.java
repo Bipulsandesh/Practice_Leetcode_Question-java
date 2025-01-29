db34dc302f28d44e6d5aa2bca7aca55a14f777c5
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2025-01-29 16:39:43 +0530

    Time: 173 ms (78.19%), Space: 101 MB (41.48%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,51 @@
+class WordDictionary {
+    class TrieNode {
+        TrieNode[] children;
+        boolean isWord;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isWord = false;
+        }
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    public boolean search(String word) {
+        return search(root, word, 0);
+    }
+
+    private boolean search(TrieNode node, String word, int index) {
+        if (index == word.length()) {
+            return node.isWord;
+        }
+        char c = word.charAt(index);
+        if (c == '.') {
+            for (TrieNode child : node.children) {
+                if (child != null && search(child, word, index + 1)) {
+                    return true;
+                }
+            }
+        } else {
+            if (node.children[c - 'a'] != null && search(node.children[c - 'a'], word, index + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
