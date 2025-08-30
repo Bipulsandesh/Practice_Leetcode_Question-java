a70d5c5f70be4cc8cc3ea30c7549f0c623ac1af7
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2025-08-30 23:17:59 +0530

    Time: 3 ms (52.39%), Space: 44.4 MB (66.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,20 +1,46 @@
-class Solution{
-        public boolean isValidSudoku(char[][] board) {
-    for(int i = 0; i<9; i++){
-        HashSet<Character> rows = new HashSet<Character>();
-        HashSet<Character> columns = new HashSet<Character>();
-        HashSet<Character> cube = new HashSet<Character>();
-        for (int j = 0; j < 9;j++){
-            if(board[i][j]!='.' && !rows.add(board[i][j]))
-                return false;
-            if(board[j][i]!='.' && !columns.add(board[j][i]))
-                return false;
-            int RowIndex = 3*(i/3);
-            int ColIndex = 3*(i%3);
-            if(board[RowIndex + j/3][ColIndex + j%3]!='.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
-                return false;
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        //for rows
+        for(int i=0;i<9;i++){
+            Set<Character> set=new HashSet<>();
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                if(set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
         }
+
+        //for cols
+        for(int j=0;j<9;j++){
+            Set<Character> set=new HashSet<>();
+            for(int i=0;i<9;i++){
+                if(board[i][j]=='.') continue;
+                if(set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
+
+        // for box
+        for(int sr=0;sr<9;sr+=3){
+            int er=sr+2;
+            for(int sc=0;sc<9;sc+=3){
+                int ec=sc+2;
+                if(!(isValid(board,sr,er,sc,ec))) return false;
+            }
+        }
+        return true;
     }
-    return true;
-}
+
+    boolean isValid(char[][]board,int sr,int er,int sc,int ec){
+        Set<Character> set=new HashSet<>();
+        for(int i=sr;i<=er;i++){
+            for(int j=sc;j<=ec;j++){
+                if(board[i][j]=='.') continue;
+
+                if(set.contains(board[i][j])) return false;
+                set.add(board[i][j]);
+            }
+        }
+        return true;
+    }
 }
