88784a26975caaf49004aef35541ac67b37ade1f
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2025-08-31 23:06:57 +0530

    Time: 184 ms (74.9%), Space: 41.4 MB (25.13%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,99 +1,53 @@
 class Solution {
-    int n;
-    int sqrt;
-    int[] rows;
-    int[] cols;
-    int[] box;
-    char[][] board;
-
-    private boolean isPerfectSquare(int n){
-        int sqrt = (int) Math.sqrt(n);
-        return sqrt*sqrt ==n;
-    }
-    private void initialize(char[][] board){
-        this.n = board.length;
-        this.sqrt = (int) Math.sqrt(n);
-        if(n != board[0].length){
-            throw new IllegalArgumentException("board must be a nxn");
-        }
-        if(!isPerfectSquare(n)){
-            throw new IllegalArgumentException("board size should be perfect square");
-        }
-        rows = new int[n];
-        cols = new int[n];
-        box = new int[n];
-        this.board = board;
-    }
-
-    private void updateInitialState(){
-        for(int i=0;  i<n; i++){
-            for(int j=0; j<n; j++){
-                if(board[i][j] != '.'){
-                    int val = board[i][j] - '0';
-                    tryStoring(val, i, j);
-                }
-            }
-        }
-    }
-
-
     public void solveSudoku(char[][] board) {
-        initialize(board);
-        updateInitialState();
-        traverse(0, 0);
+        helper(board, 0, 0);
+        return;
     }
-
-    private boolean tryStoring(int val, int r, int c){
-        int valBitPos = 1<<(val-1);
-        int k = (r/sqrt)*(sqrt) + c/sqrt; //box id or index
-        if( (rows[r] & valBitPos) > 0 ||
-            (cols[c] & valBitPos) > 0 || 
-            (box[k] & valBitPos) > 0){
-                return false;
-            }
-
-            rows[r] |= valBitPos;
-            cols[c] |= valBitPos;
-            box[k]  |= valBitPos;
-            board[r][c] = Character.forDigit(val, 10);
-            return true;
-    }
-
-    private void unStore(int val, int r, int c){
-        board[r][c] = '.';
-        int k = (r/sqrt)*sqrt + c/sqrt;
-        int valBitPos = 1<<(val-1);
-        rows[r] &= ~valBitPos;
-        cols[c] &= ~valBitPos;
-        box[k] &= ~valBitPos;
-    }
-
-    private boolean moveNext(int r, int c){
-        if(r==n-1 && c==n-1) {
+    private static boolean helper(char[][] board, int row, int col){
+        if(row == 9){
             return true;
         }
-        if(c==n-1){
-            return traverse(r+1,0);
+        int nextRow = row; 
+        int nextCol = col + 1;
+        if(nextCol == 9){
+            nextRow = row + 1;
+            nextCol = 0;
         }
-        else{
-            return traverse(r,c+1);
+        if(board[row][col] != '.'){
+            return helper(board, nextRow, nextCol);
         }
-    }
-
-    public boolean traverse(int r, int c){
-        if(board[r][c] != '.'){
-            return moveNext(r,c);
-        }
-        
-        for(int v=1;v<=n;v++){
-            if(tryStoring(v,r,c)){
-                if(moveNext(r,c)){
+        for(int dig = 1; dig <= 9; dig++){
+            if(isSafe(board, row, col, dig)){
+                board[row][col] = (char)(dig + '0');
+                if(helper(board, nextRow, nextCol)){
                     return true;
                 }
-                unStore(v,r,c);
+                board[row][col] = '.';
             }
-                
         }
         return false;
+    }
+    private static boolean isSafe(char[][] board, int row, int col, int dig){
+        for(int i = 0; i<9; i++){
+            if(board[row][i] == (char)(dig + '0')){
+                return false;
+            }
+        }
+        for(int i = 0; i<9; i++){
+            if(board[i][col] == (char)(dig + '0')){
+                return false;
+            }
+        }
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+
+        for(int i = sr; i <= sr+2; i++){
+            for(int j = sc; j <= sc+2; j++){
+                if(board[i][j] == (char)(dig + '0')){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
