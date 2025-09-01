fe0fcf9a6bc685e7894284c5e262c5041b223cd9
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2025-09-01 22:06:30 +0530

    Time: 336 ms (69.43%), Space: 105.9 MB (6.74%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -1,54 +1,30 @@
 class Solution {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        PriorityQueue<ClassRecord> pq = new PriorityQueue<>(new Compare());
-        
-        for(int[] cl : classes)
-            pq.add(new ClassRecord(cl));
-        
-        ClassRecord cl;
-        while(extraStudents-- > 0)
-            pq.add(pq.remove().addOneStudent());
-        
-        double sum = 0;
-        while(!pq.isEmpty()){
-            cl = pq.remove();
-            sum += (double)cl.pass / cl.total;
+        int n = classes.length;
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+        for (int i = 0; i < n; i++) {
+            double pr = (double) classes[i][0] / classes[i][1];
+            double newpr = (double) (classes[i][0] + 1) / (classes[i][1] + 1);
+            double delta = newpr - pr;
+            pq.offer(new double[] { delta, i });
         }
+        while (extraStudents > 0) {
+            double[] temp = pq.poll();
+            double delta = temp[0];
+            int idx = (int) temp[1];
+            classes[idx][0]++;
+            classes[idx][1]++;
+            double pr = (double) classes[idx][0] / classes[idx][1];
+            double newpr = (double) (classes[idx][0] + 1) / (classes[idx][1] + 1);
+            delta = newpr - pr;
+            pq.offer(new double[] { delta, idx });
+            extraStudents--;
 
-        return sum / classes.length;
-    }
-}
-
-class ClassRecord{
-    int pass;
-    int total;
-    double inc;
-
-    public ClassRecord(int[] array){
-        pass = array[0];
-        total = array[1];
-        inc = getIncrement();
-    }
-
-    public ClassRecord addOneStudent(){
-        pass++;
-        total++;
-        inc = getIncrement();
-        return this;
-    }
-
-    private double getIncrement(){
-        return (pass + 1.0) / (total + 1) - (double)pass / total;
-    }
-}
-
-class Compare implements Comparator<ClassRecord>{
-    public int compare(ClassRecord a, ClassRecord b){
-        if(a.inc < b.inc)
-            return 1;
-        else if(a.inc > b.inc)
-            return -1;
-        else
-            return 0;
+        }
+        double result = 0.0;
+        for (int i = 0; i < n; i++) {
+            result += (double) classes[i][0] / classes[i][1];
+        }
+        return result / n;
     }
 }
