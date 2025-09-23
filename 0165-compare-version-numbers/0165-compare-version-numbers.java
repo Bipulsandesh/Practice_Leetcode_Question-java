0195ac726d5fbee9f6e6cd69cb39bc372cfd5f8c
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2025-09-23 22:50:38 +0530

    Time: 0 ms (100%), Space: 41.6 MB (67.13%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,25 +1,27 @@
+// Java Optimal: Two pointers + On-the-fly Integer Parse
 class Solution {
     public int compareVersion(String version1, String version2) {
-        int i = 0, j = 0;
+        int i1 = 0, i2 = 0;
+        int n1 = version1.length(), n2 = version2.length();
 
-        while (i < version1.length() || j < version2.length()) {
+        while (i1 < n1 || i2 < n2) {
             int v1 = 0, v2 = 0;
 
-            while (i < version1.length() && version1.charAt(i) != '.') {
-                v1 = v1 * 10 + (version1.charAt(i) - '0');
-                i++;
+            while (i1 < n1 && version1.charAt(i1) != '.') {
+                v1 = v1 * 10 + (version1.charAt(i1) - '0');
+                i1++;
             }
 
-            while (j < version2.length() && version2.charAt(j) != '.') {
-                v2 = v2 * 10 + (version2.charAt(j) - '0');
-                j++;
+            while (i2 < n2 && version2.charAt(i2) != '.') {
+                v2 = v2 * 10 + (version2.charAt(i2) - '0');
+                i2++;
             }
 
+            if (v1 < v2) return -1;
             if (v1 > v2) return 1;
-            else if (v1 < v2) return -1;
 
-            i++;
-            j++;
+            i1++; // skip '.'
+            i2++;
         }
 
         return 0;
