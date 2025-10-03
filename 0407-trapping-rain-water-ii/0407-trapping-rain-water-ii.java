ab30f09b7e74cca25f2fe3fa1da1ad9a3fba989a
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2025-10-03 21:00:01 +0530

    Time: 20 ms (51.73%), Space: 46.3 MB (71.37%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -1,62 +1,38 @@
 class Solution {
-
+    private boolean isValid(int x,int y,int n,int m){
+        return x>=0&&y>=0&&x<n&&y<m;
+    }
     public int trapRainWater(int[][] heightMap) {
-        //left,right,down, up
-        int dir[][] = {{0,-1},{0,1},{-1,0},{1,0}};
-        int rows = heightMap.length;
-        int cols = heightMap[0].length;
-        if(rows <3 || cols <3) return 0;
-        int totalUnvisitedCells = rows*cols;
-        boolean[][] visited = new boolean[rows][cols];
-        // [hight, row, col] -> inc order of height
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0]-b[0]));
-        
-        for (int i = 0; i < rows; i++) {
-            pq.offer(new int[]{heightMap[i][0], i, 0});
-            pq.offer(new int[]{heightMap[i][cols - 1], i, cols - 1});
-            visited[i][0] = true;
-            visited[i][cols - 1] = true;
-            totalUnvisitedCells--;
-            totalUnvisitedCells--;
+        int n=heightMap.length,m=heightMap[0].length;
+        int water=0;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((x,y)->x[0]-y[0]);
+        boolean[][] vis=new boolean[n][m];
+        for(int i=0;i<n;i++){
+            pq.offer(new int[]{heightMap[i][0],i,0});
+            pq.offer(new int[]{heightMap[i][m-1],i,m-1});
+            vis[i][0]=true;
+            vis[i][m-1]=true;
         }
-
-        for (int i = 1; i < cols-1; i++) {
-            pq.offer(new int[]{heightMap[0][i], 0, i});
-            pq.offer(new int []{heightMap[rows - 1][i], rows - 1, i});
-            visited[0][i] = true;
-            visited[rows - 1][i] = true;
-            totalUnvisitedCells--;
-            totalUnvisitedCells--;
+        for(int i=1;i<m-1;i++){
+            pq.offer(new int[]{heightMap[0][i],0,i});
+            pq.offer(new int[]{heightMap[n-1][i],n-1,i});
+            vis[0][i]=true;
+            vis[n-1][i]=true;
         }
-        int trappedWater = 0;
-        int waterLevel=0;
-        //n*mlog(n*m)
-        while (!pq.isEmpty() && totalUnvisitedCells>0) {
-            int currentCell[] = pq.poll();
-            int currentHeight = currentCell[0];
-            int currentRow = currentCell[1];
-            int currentCol = currentCell[2];
-            waterLevel = Math.max(waterLevel, currentHeight);
-            // Explore all 4 neighboring cells
-            for (int direction = 0; direction < 4; direction++) {
-                int neighborRow = currentRow + dir[direction][0];
-                int neighborCol = currentCol + dir[direction][1];
-                // Check if the neighbor is within the grid bounds and not yet visited
-                if (isValidCell(neighborRow,neighborCol,rows,cols) && !visited[neighborRow][neighborCol]) {
-                    int neighborHeight = heightMap[neighborRow][neighborCol];
-                    if (neighborHeight < waterLevel) {
-                        trappedWater += waterLevel - neighborHeight;
-                    }
-                    pq.offer(new int[]{neighborHeight,neighborRow,neighborCol});
-                    visited[neighborRow][neighborCol] = true;
-                    totalUnvisitedCells--;
+        int[] a={1,-1,0,0};
+        int[] b={0,0,1,-1};
+        while(!pq.isEmpty()){
+            int[] temp=pq.poll();
+            for(int i=0;i<4;i++){
+                int x=temp[1]+a[i];
+                int y=temp[2]+b[i];
+                if(isValid(x,y,n,m)&&!vis[x][y]){
+                    water+=Math.max(0,temp[0]-heightMap[x][y]);
+                    pq.offer(new int[]{Math.max(temp[0],heightMap[x][y]),x,y});
+                    vis[x][y]=true;
                 }
             }
         }
-        return trappedWater;
-    }
-
-    private boolean isValidCell(int row,int col,int rows,int cols) {
-        return row >= 0 && col >= 0 && row < rows && col < cols;
+        return water;
     }
 }
