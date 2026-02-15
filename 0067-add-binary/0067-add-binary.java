5f953ca1f0c37cb4d15a7f0cd6937915aea42dce
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2026-02-15 11:17:01 +0530

    Time: 1 ms (99.65%), Space: 43.8 MB (38.26%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,34 +1,21 @@
-class Solution {
-   public static String addBinary(String a, String b) {
-        // Initialize result as a StringBuilder for efficiency
-        StringBuilder result = new StringBuilder();
-        
-        // Start from the end of both strings
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int carry = 0;
+class Solution 
+{
+  public String addBinary(String a, String b) 
+  {
+    StringBuilder sb = new StringBuilder();
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
 
-        // Traverse both strings from right to left
-        while (i >= 0 || j >= 0 || carry != 0) {
-            // Get the current bits (0 if out of bounds)
-            int bitA = (i >= 0) ? a.charAt(i) - '0' : 0;
-            int bitB = (j >= 0) ? b.charAt(j) - '0' : 0;
-
-            // Calculate the sum of bits and carry
-            int sum = bitA + bitB + carry;
-
-            // Append the sum modulo 2 to result (0 or 1)
-            result.append(sum % 2);
-
-            // Update carry (sum divided by 2)
-            carry = sum / 2;
-
-            // Move to the next bit
-            i--;
-            j--;
-        }
-
-        // Reverse the result since we built it backwards
-        return result.reverse().toString();
+    while (i >= 0 || j >= 0 || carry == 1) 
+    {
+      if(i >= 0)
+        carry += a.charAt(i--) - '0';
+      if(j >= 0)
+        carry += b.charAt(j--) - '0';
+      sb.append(carry % 2);
+      carry /= 2;
     }
+    return sb.reverse().toString();
+  }
 }
