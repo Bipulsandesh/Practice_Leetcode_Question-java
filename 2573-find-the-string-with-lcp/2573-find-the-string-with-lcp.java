d1f6e3d2a58fa5fb22bc964474b25b475363fbb7
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2026-03-28 12:18:46 +0530

    Time: 35 ms (17.14%), Space: 192.6 MB (8.57%) - LeetHub

---
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
@@ -0,0 +1,78 @@
+class Solution {
+    public String findTheString(int[][] lcp) {
+        int n = lcp.length;
+
+        // Step 1: Validate diagonal
+        for (int i = 0; i < n; i++) {
+            if (lcp[i][i] != n - i) return "";
+        }
+
+        // DSU setup
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        // Find function
+        java.util.function.IntUnaryOperator find = new java.util.function.IntUnaryOperator() {
+            public int applyAsInt(int x) {
+                if (parent[x] != x)
+                    parent[x] = this.applyAsInt(parent[x]);
+                return parent[x];
+            }
+        };
+
+        // Union function
+        java.util.function.BiConsumer<Integer, Integer> union = (a, b) -> {
+            int pa = find.applyAsInt(a);
+            int pb = find.applyAsInt(b);
+            if (pa != pb) parent[pa] = pb;
+        };
+
+        // Step 2: Union where lcp > 0
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (lcp[i][j] > 0) {
+                    union.accept(i, j);
+                }
+            }
+        }
+
+        // Step 3: Assign characters
+        char[] res = new char[n];
+        java.util.Map<Integer, Character> map = new java.util.HashMap<>();
+        char curr = 'a';
+
+        for (int i = 0; i < n; i++) {
+            int root = find.applyAsInt(i);
+            if (!map.containsKey(root)) {
+                if (curr > 'z') return ""; // more than 26 groups
+                map.put(root, curr++);
+            }
+            res[i] = map.get(root);
+        }
+
+        String s = new String(res);
+
+        // Step 4: Validate LCP matrix
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (i + 1 < n && j + 1 < n) {
+                        dp[i][j] = 1 + dp[i + 1][j + 1];
+                    } else {
+                        dp[i][j] = 1;
+                    }
+                } else {
+                    dp[i][j] = 0;
+                }
+
+                if (dp[i][j] != lcp[i][j]) {
+                    return "";
+                }
+            }
+        }
+
+        return s;
+    }
+}
