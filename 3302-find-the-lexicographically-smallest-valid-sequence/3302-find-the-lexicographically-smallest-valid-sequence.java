3f859623dd080680d618555eaeb8cfb38ac7fdb4
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2026-08-08 11:56:17 +0530

    Time: 33 ms (80.56%), Space: 137.5 MB (52.78%) - LeetHub

---
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] validSequence(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[] res = new int[n];
+        int[] rightMatch = new int[n];
+
+        int i = m - 1, j = n - 1;
+        while (i >= 0 && j >= 0) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                rightMatch[j] = i;
+                j--;
+            }
+            i--;
+        }
+
+        i = 0; 
+        j = 0;
+        boolean changed = false;
+        
+        while (i < m && j < n) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                res[j] = i;
+                j++;
+            } else if (!changed && (j == n - 1 || rightMatch[j + 1] > i)) {
+                changed = true;
+                res[j] = i;
+                j++;
+            }
+            i++;
+        }
+        
+        return j == n ? res : new int[]{};
+    }
+}
