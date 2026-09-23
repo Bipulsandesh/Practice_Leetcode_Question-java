392fb54952e8c80273b2cdedd8b075858a5464dd
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2026-09-23 14:25:35 +0530

    Time: 233 ms (5.07%), Space: 102 MB (59.92%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,38 +1,34 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+        int n=nums.length;
+        //HashMap<Integer>mp=new HashMap<>();
+        long sum=0;
+        for(int num:nums){
+            sum+=num;
         }
-        int target = totalSum - x;
-         if (target < 0) {
+        long target=sum-x;
+        if(target<0){
             return -1;
         }
-       if (target == 0) {
-            return nums.length;
+        if(target==0){
+            return n;
         }
-        
-        int maxLen = -1;
-        int currentSum = 0;
-        int left = 0;
-        
-       
-        for (int right = 0; right < nums.length; right++) {
-            currentSum += nums[right];
-            
-            
-            while (currentSum > target && left <= right) {
-                currentSum -= nums[left];
-                left++;
+        int low=0;
+        int windowsum=0;
+        int len=-1;
+        for(int high=0;high<n;high++){
+            windowsum+=nums[high];
+            while(low<=high && windowsum>target){
+                windowsum-=nums[low];
+                low++;
             }
-            
-            
-            if (currentSum == target) {
-                maxLen = Math.max(maxLen, right - left + 1);
+            if(windowsum==target){
+                len=Math.max(len,high-low+1);
             }
         }
-        
-        
-        return maxLen != -1 ? nums.length - maxLen : -1;
+        if(len==-1){
+            return -1;
+        }
+        return n-len;
     }
 }
